3017b326d9c85663de5159c8b18d5b6a6e9f318c
Sarah Ella Stoltze <sstoltze@gmail.com>
2026-01-13 16:48:13 +0100

    Fantasticon-conclar build v2

---
+++ 2026/people.js
@@ -0,0 +1,18 @@
+[
+  {
+    "id": "1",
+    "name": [
+      "Sarah Stoltze"
+    ],
+    "sortname": "Stoltze Sarah",
+    "bio": "Sarah is not good at web stuff."
+  },
+  {
+    "id": "2",
+    "name": [
+      "Sanna Bo Claumarch"
+    ],
+    "sortname": "Claumarch Sanna Bo",
+    "bio": "Extremely cool."
+  }
+]
---
+++ 2026/program.js
@@ -0,0 +1,20 @@
+[
+  {
+    "id": "1",
+    "title": "Opening Ceremony",
+    "date": "2026-06-05",
+    "time": "10:00",
+    "mins": "45",
+    "desc": "A ceremony",
+    "people": [
+      {
+        "id": "1"
+      },
+      {
+        "id": "2"
+      }
+    ],
+    "tags": [],
+    "loc": []
+  }
+]
