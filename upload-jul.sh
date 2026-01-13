6cb9e399364434da8b1ac18b590862f67ac02e8a
Sarah Ella Stoltze <sstoltze@gmail.com>
2026-01-13 16:16:52 +0100

    Remove old site

--- upload-jul.sh
+++
@@ -1,3 +0,0 @@
-scp -pCr jul/ sstoltze@ssh1.imf.au.dk:~/public_html/
-ssh sstoltze@ssh1.imf.au.dk "cd public_html; cp -r jul/julebilleder .; chmod -R -v 704 *.html *.png jul julebilleder; chmod a+x ./jul/julekalender.sh; sh ./jul/julekalender.sh; chmod o+x jul julebilleder jul/julebilleder;"
-
