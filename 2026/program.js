d920b3c5a9274ab3d529e783f25d332fd171df97
Sarah Ella Stoltze <sstoltze@gmail.com>
2026-05-06 09:42:53 +0200

    Update the programme

--- 2026/program.js
+++ 2026/program.js
@@ -1,20 +1,530 @@
 [
   {
-    "id": "1",
-    "title": "Opening Ceremony",
-    "date": "2026-06-05",
-    "time": "10:00",
-    "mins": "45",
-    "desc": "A ceremony",
-    "people": [
-      {
-        "id": "1"
-      },
-      {
-        "id": "2"
-      }
+    "date": "2026-06-07",
+    "id": "28719",
+    "loc": [
+      "Store sal 3. sal"
     ],
+    "mins": "45",
+    "people": [],
     "tags": [],
-    "loc": []
+    "time": "17:00",
+    "title": "Dead Dog Party"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "76077",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "16:00",
+    "title": "Farvel og tak - fra alle os"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "53977",
+    "loc": [
+      "Lille sal Stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "16:00",
+    "title": "Oprydning"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "75492",
+    "loc": [
+      "Mødelokale 2. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "16:00",
+    "title": "Oprydning"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "36554",
+    "loc": [
+      "Mødelokale stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "16:00",
+    "title": "Oprydning"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "41608",
+    "loc": [
+      "Biblioteket"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "16:00",
+    "title": "Oprydning"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "66471",
+    "loc": [
+      "Cafe"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "16:00",
+    "title": "Oprydning"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "32264",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "14:00",
+    "title": "Gross Love - love in horror"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "72800",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "13:00",
+    "title": "Interview med Chris Beckett"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "81316",
+    "loc": [
+      "Mødelokale stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "13:00",
+    "title": "Workshop: Reading Journals"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "74052",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "12:00",
+    "title": "Lunarpunk"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "36550",
+    "loc": [
+      "Lille sal Stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "12:00",
+    "title": "On writing romantasy"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "34251",
+    "loc": [
+      "Mødelokale 2. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "12:00",
+    "title": "kaffe med en gæst"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "55389",
+    "loc": [
+      "Mødelokale stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "12:00",
+    "title": "kaffe med en gæst"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "50975",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "11:00",
+    "title": "Kinesisk fantasy (måske Queer kineisisk fantasy)"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "69217",
+    "loc": [
+      "Lille sal Stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "11:00",
+    "title": "Foredrag Kim Escherich - The White Man"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "36483",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "10:00",
+    "title": "Adaptations - fra bog til film, tegneserier til film, podcast til film"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "78996",
+    "loc": [
+      "Lille sal Stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "10:00",
+    "title": "Vi ❤️ Science fiction"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "15372",
+    "loc": [
+      "Cafe"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "10:00",
+    "title": "QUIZ (lyd)"
+  },
+  {
+    "date": "2026-06-07",
+    "id": "38401",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "09:00",
+    "title": "Dørene åbner"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "57198",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "18:00",
+    "title": "Auktion (Klaus Æ Mogensen)"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "98660",
+    "loc": [
+      "Lille sal Stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "18:00",
+    "title": "Merlin og Monopolet - dilemmaer i fantasyland"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "5283",
+    "loc": [
+      "Mødelokale 2. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "18:00",
+    "title": "Spil"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "89966",
+    "loc": [
+      "Mødelokale stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "18:00",
+    "title": "Meetup"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "24394",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "17:00",
+    "title": "Auktion (Klaus Æ Mogensen)"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "56754",
+    "loc": [
+      "Lille sal Stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "17:00",
+    "title": "Merlin og Monopolet - dilemmaer i fantasyland"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "61168",
+    "loc": [
+      "Mødelokale 2. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "17:00",
+    "title": "Spil"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "39903",
+    "loc": [
+      "Mødelokale stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "17:00",
+    "title": "Meetup"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "70203",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "16:00",
+    "title": "\"Love, Power, Sex, and Addiction in the writings of Octavia E. Butler\""
+  },
+  {
+    "date": "2026-06-06",
+    "id": "89130",
+    "loc": [
+      "Lille sal Stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "16:00",
+    "title": "Arthurmyterne i litteraturen "
+  },
+  {
+    "date": "2026-06-06",
+    "id": "11585",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "15:00",
+    "title": "Interview med Sarah Pinsker"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "12434",
+    "loc": [
+      "Lille sal Stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "15:00",
+    "title": "Vi🫀Horror"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "32663",
+    "loc": [
+      "Lille sal Stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "14:00",
+    "title": "Tamora Pierces forfatterskab"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "2085",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "13:00",
+    "title": "De hugo-nominerede 2026"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "13026",
+    "loc": [
+      "Lille sal Stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "13:00",
+    "title": "Flammesværdet ... Hele serien"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "97501",
+    "loc": [
+      "Mødelokale 2. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "13:00",
+    "title": " "
+  },
+  {
+    "date": "2026-06-06",
+    "id": "92694",
+    "loc": [
+      "Mødelokale stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "13:00",
+    "title": "kaffe med en gæst"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "56146",
+    "loc": [
+      "Biblioteket"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "13:00",
+    "title": " "
+  },
+  {
+    "date": "2026-06-06",
+    "id": "17808",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "12:00",
+    "title": "Interview med Eugenia T"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "95824",
+    "loc": [
+      "Mødelokale 2. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "12:00",
+    "title": "Speeddate en bog"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "15362",
+    "loc": [
+      "Lille sal Stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "11:00",
+    "title": "Vi ❤️ Fantasy"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "38772",
+    "loc": [
+      "Mødelokale stuen"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "11:00",
+    "title": "Møde mellem fans - mød en ny ven"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "41480",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "10:00",
+    "title": "Velkommen - plus introduktion til æresgæster"
+  },
+  {
+    "date": "2026-06-06",
+    "id": "91184",
+    "loc": [
+      "Store sal 3. sal"
+    ],
+    "mins": "45",
+    "people": [],
+    "tags": [],
+    "time": "09:00",
+    "title": "Dørene åbner"
   }
 ]
